af599fc7cb3e29e7b71c5998c8d5a3854beb48b5
Yewon Jung <144860619+eh-ong@users.noreply.github.com>
2024-03-12 14:47:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 18.24 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public List<Integer> solution(int[] arr) {
+                
+        List<Integer> answer = new ArrayList<>();
+        
+        
+        answer.add(arr[0]);
+        
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i-1] != arr[i]) {
+                answer.add(arr[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
