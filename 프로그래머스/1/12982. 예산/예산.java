ff8a080b0a4030374b0ed7c3ddeb0d9518173d9b
Yewon Jung <144860619+eh-ong@users.noreply.github.com>
2024-03-15 03:24:44 +0900

    [level 1] Title: 예산, Time: 0.45 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+    
+class Solution {
+    public int solution(int[] d, int budget) {
+        
+        Arrays.sort(d);
+        
+        int answer = 0;
+        
+        for(int i = 0 ; i < d.length ; i++) {
+            if(budget >= d[i]) {
+                budget -= d[i];
+                answer++;
+            }
+            if(budget == 0) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
