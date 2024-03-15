613e0680846a5e5d9042e026ad5b1a069eb05156
Yewon Jung <144860619+eh-ong@users.noreply.github.com>
2024-03-16 03:54:14 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        
+        int answer = 0;
+        
+        int[] students = new int[n];
+        
+        for(int i = 0 ; i < students.length ; i++) {
+            students[i] = 1;
+        }
+        
+        for(int i = 0 ; i < lost.length ; i++) {
+            int num = lost[i];
+            students[num-1] = 0;
+        }
+        
+        for(int i = 0 ; i < reserve.length ; i++) {
+            int num = reserve[i];
+            students[num-1]++;
+        }
+        
+        for(int i = 0 ; i < students.length ; i++) {
+            if(i == 0) {
+                if(students[i] == 0 && students[i+1] == 2) {
+                    students[i] = 1;
+                    students[i+1] = 1;
+                }
+            } else if(i == students.length-1) {
+                if(students[i] == 0 && students[i-1] ==2) {
+                    students[i] = 1;
+                    students[i-1] = 1;
+                }
+            } else {
+                if(students[i] == 0) {
+                    if(students[i-1] == 2) {
+                        students[i] = 1;
+                        students[i-1] = 1;
+                    } else if(students[i+1] == 2) {
+                        students[i] = 1;
+                        students[i+1] = 1;
+                    }
+                }
+            }
+            
+        }
+        
+        for(int i = 0 ; i < students.length ; i++) {
+            if(students[i] == 1 || students[i] == 2) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
